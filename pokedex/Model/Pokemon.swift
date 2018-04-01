b14b711ab4e846cbe46bb196b19412dddc852658
Andrew Miller <milldawg@github.com>
2018-04-01 23:50:32 +0100

    Created a basic UICollectionView, added CSV parser, and assets too

---
+++ pokedex/Model/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Andrew Miller on 01/04/2018.
+//  Copyright © 2018 Andrew Miller. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        
+        self._name = name
+        self._pokedexID = pokedexID
+        
+    }
+    
+}
---
+++ pokedex/View/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Andrew Miller on 01/04/2018.
+//  Copyright © 2018 Andrew Miller. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+    
+}
